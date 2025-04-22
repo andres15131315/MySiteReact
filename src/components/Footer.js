25bc71af64c8c7883199b0263fef220ade97157f
andres <asve1315@gmail.com>
2025-04-21 21:40:57 -0500

    Creación de componentes y estructura

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function Footer() {
+  return (
+   <footer className='footer'>
+<p>&copy; 2025 My sitio 2025. Todos los derechos reservados</p>
+
+   </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Navbar() {
+  return (
+    <div className='navbar'>
+
+     <a>Servicios</a>
+     <a>Usuarios</a>
+     <a>Contacto</a>
+    </div>
+  )
+}
+
+export default Navbar;
